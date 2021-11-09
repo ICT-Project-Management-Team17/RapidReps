546743559ebbeddec2740a61185215f9663d9ade
Kamran <56407135+k5924@users.noreply.github.com>
2021-11-09 11:37:54 +0000

    RR-91: found placement for center widget

--- lib/screens/initial_signup_screen.dart
+++ lib/screens/initial_signup_screen.dart
@@ -10,13 +10,28 @@ class InitialSignUpScreen extends StatefulWidget {
 class _InitialSignUpScreenState extends State<InitialSignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/london.png"),
-          fit: BoxFit.cover,
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/london.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              child: Text(
+                'Center Widget',
+                textScaleFactor: 3,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
         ),
       ),
     );
