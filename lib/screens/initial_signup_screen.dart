8c70daea43fdac35483779142e1383ca372c046c
Kamran <56407135+k5924@users.noreply.github.com>
2021-11-09 12:39:37 +0000

    RR-91: Updated content to look like the design

--- lib/screens/initial_signup_screen.dart
+++ lib/screens/initial_signup_screen.dart
@@ -24,13 +24,109 @@ class _InitialSignUpScreenState extends State<InitialSignUpScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              color: Colors.white,
-              child: Text(
-                'Center Widget',
-                textScaleFactor: 3,
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25.0),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 36.0,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color(0xFF009FE3),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 70,
+                            vertical: 10,
+                          ),
+                          textStyle: const TextStyle(fontSize: 20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                        ),
+                        child: const Text(
+                          'I Am A CDO',
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color(0xFF009B14),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 60,
+                            vertical: 10,
+                          ),
+                          textStyle: const TextStyle(fontSize: 20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                        ),
+                        child: const Text(
+                          'I Am A Solicitor',
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color(0xFF951B81),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 10,
+                          ),
+                          textStyle: const TextStyle(fontSize: 20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                        ),
+                        child: const Text(
+                          'I Am A Firm Representative',
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color(0xFFE30613),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 105,
+                            vertical: 10,
+                          ),
+                          textStyle: const TextStyle(fontSize: 20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                        ),
+                        child: const Text(
+                          'Back',
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                )),
           ],
         ),
       ),
