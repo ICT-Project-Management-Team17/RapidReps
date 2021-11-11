ec7dd574c414606f321391ad82355984c350c567
Kamran <56407135+k5924@users.noreply.github.com>
2021-11-11 15:40:11 +0000

    RR-96: started building CDO agreemant page

--- lib/screens/cdo_agreemant_screen.dart
+++ lib/screens/cdo_agreemant_screen.dart
@@ -13,17 +13,37 @@ class _CDOAgreemantScreenState extends State<CDOAgreemantScreen> {
     return Scaffold(
       body: Center(
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 120.0),
-                  child: Text(
-                    "THIS IS THE CDO AGREEMANT PAGE WHICH IS CURRENTLY UDNER CONSTRUCTION",
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Color(0xFF009FE3),
+                    width: 5,
+                  ),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(25.0),
                   ),
                 ),
-              ),
-            ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    children: const [
+                      Text(
+                        "CDO Agreemant Page",
+                        style: TextStyle(
+                          fontSize: 25.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                    ],
+                  ),
+                )),
+          ],
+        ),
       ),
     );
   }
