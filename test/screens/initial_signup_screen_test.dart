4419f7d378e3fbd89f7405fa3659971df69ce50e
Kamran <56407135+k5924@users.noreply.github.com>
2021-11-15 14:09:21 +0000

    When CDO button is clicked, user is redirected to correct agreemant screen

--- test/screens/initial_signup_screen_test.dart
+++ test/screens/initial_signup_screen_test.dart
@@ -1,13 +1,41 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:rapid_reps/screens/export.dart';
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
   group(
     'Initial Sign Up Screen: ',
     () {
       testWidgets(
-        'When Sign Up Button is clicked, user is redirected to Sign Up Page',
-        (WidgetTester tester) async {},
-      );
+          'When CDO Button is clicked, user is redirected to the CDO agreement screen',
+          (WidgetTester tester) async {
+        final mockObserver = MockNavigatorObserver();
+        await tester.pumpWidget(
+          MaterialApp(
+            home: const InitialSignUpScreen(),
+            navigatorObservers: [mockObserver],
+          ),
+        );
+
+        await tester.tap(
+          find.text('CDO'),
+        );
+        await tester.pumpAndSettle();
+
+        expect(
+          find.byType(AgreemantScreen),
+          findsOneWidget,
+        );
+
+        expect(
+            (tester.firstWidget(find.byType(FloatingActionButton))
+                    as FloatingActionButton)
+                .backgroundColor,
+            const Color(0xFF009FE3));
+      });
     },
   );
 }
