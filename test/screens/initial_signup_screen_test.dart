b1f69dd99f9415e3ed064536da236bfab3d8aa31
Kamran <56407135+k5924@users.noreply.github.com>
2021-11-15 13:53:16 +0000

    Initialize test for initial sign up screen

---
+++ test/screens/initial_signup_screen_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'Initial Sign Up Screen: ',
+    () {
+      testWidgets(
+        'When Sign Up Button is clicked, user is redirected to Sign Up Page',
+        (WidgetTester tester) async {},
+      );
+    },
+  );
+}
