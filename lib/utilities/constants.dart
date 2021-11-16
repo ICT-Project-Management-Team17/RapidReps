81fc062368a83ba7c96a008b80b44feaceceff61
Kamran <56407135+k5924@users.noreply.github.com>
2021-11-16 16:42:13 +0000

    Refactor: made colours more modifiable

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 String kTerms =
     '''1. By agreeing to these terms, users must have read and agreed to the Privacy and Data Policies, which explain how we collect and use data provided by the users.
 2. By agreeing to these terms, you possess the right qualifications to use the service this application offers.
@@ -5,3 +7,7 @@ String kTerms =
 4. Rapid Reps service is only provided to users within the UK, to be used in countries where English law is governing.
 5. Users must act carefully when handling private information obtained via Rapid Reps, this information must not be disclosed to unauthorised personnel.
 6. Any breach to this agreement will be deemed as a violation to Rapid Reps terms of use which is punishable by account termination and may lead to legal action.''';
+
+Color kCDOColour = const Color(0xFF009FE3);
+Color kSolicitorColour = const Color(0xFF009B14);
+Color kFirmRepColour = const Color(0xFF951B81);
