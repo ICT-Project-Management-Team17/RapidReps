e58335aed0c26195e0f5c3bf1f55b6dc7eb7f905
hassan848 <hassanaslam786@hotmail.co.uk>
2021-11-23 13:17:57 +0000

    Integrated different user roles for Accounts & Logout functionality

---
+++ lib/screens/solicitor_dashboard.dart
@@ -0,0 +1,73 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:rapid_reps/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rapid_reps/screens/export.dart';
+import 'package:rapid_reps/utilities/constants.dart';
+
+class SolicitorDashboard extends StatefulWidget {
+  final userModel currentUser;
+
+  const SolicitorDashboard({Key? key, required this.currentUser})
+      : super(key: key);
+
+  @override
+  _SolicitorDashboardState createState() => _SolicitorDashboardState();
+}
+
+class _SolicitorDashboardState extends State<SolicitorDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard ${widget.currentUser.userType}'),
+        centerTitle: true,
+        backgroundColor: kSolicitorColour,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Hello",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                "${widget.currentUser.firstName} ${widget.currentUser.lastName}",
+                style: const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.w500),
+              ),
+              Text(
+                "${widget.currentUser.email}",
+                style: const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ActionChip(
+                  label: const Text("Logout"),
+                  onPressed: () {
+                    logout(context);
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const LoginScreen()));
+  }
+}
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 String kTerms =
     '''1. By agreeing to these terms, users must have read and agreed to the Privacy and Data Policies, which explain how we collect and use data provided by the users.
-2. By agreeing to these terms, you possess the right qualifications to use the service this application offers.
+2. By agreeing to these terms, you confirm that you possess the correct qualifications to use the service this application offers.
 3. All users must conduct themselves professionally in correspondents with their legal duties, any behaviour outside of this scope will be investigated and may result in account termination.
 4. Rapid Reps service is only provided to users within the UK, to be used in countries where English law is governing.
 5. Users must act carefully when handling private information obtained via Rapid Reps, this information must not be disclosed to unauthorised personnel.
