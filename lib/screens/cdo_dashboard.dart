56def49212983feff0d05a23cfc8e309d4151381
Kamran <56407135+k5924@users.noreply.github.com>
2021-11-27 17:12:06 +0000

    RR-112: added navigation bar to CDO

--- lib/screens/cdo_dashboard.dart
+++ lib/screens/cdo_dashboard.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:rapid_reps/models/user_model.dart';
 import 'package:rapid_reps/screens/export.dart';
 import 'package:rapid_reps/utilities/constants.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
 class CDODashboard extends StatefulWidget {
   final userModel currentUser;
@@ -15,6 +16,15 @@ class CDODashboard extends StatefulWidget {
 }
 
 class _CDODashboardState extends State<CDODashboard> {
+  int _currentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,41 +33,129 @@ class _CDODashboardState extends State<CDODashboard> {
         centerTitle: true,
         backgroundColor: kCDOColour,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(25),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                "Hello",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            const Center(
+              child: Text("Jobs Page"),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(25),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "${widget.currentUser.firstName} ${widget.currentUser.lastName}",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 32,
+                      ),
+                    ),
+                    Text(
+                      "${widget.currentUser.email}",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "${widget.currentUser.mobileNumber}",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "${widget.currentUser.telephoneNumber}",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "${widget.currentUser.policeStation}",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                "${widget.currentUser.firstName} ${widget.currentUser.lastName}",
-                style: const TextStyle(
-                    color: Colors.black, fontWeight: FontWeight.w500),
-              ),
-              Text(
-                "${widget.currentUser.email}",
-                style: const TextStyle(
-                    color: Colors.black, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              ActionChip(
+            ),
+            Center(
+                child: Column(
+              children: [
+                const Text("Logout"),
+                const SizedBox(
+                  height: 15,
+                ),
+                ActionChip(
                   label: const Text("Logout"),
                   onPressed: () {
                     logout(context);
-                  }),
-            ],
-          ),
+                  },
+                ),
+              ],
+            )),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        showElevation: true,
+        itemCornerRadius: 24,
+        curve: Curves.easeIn,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.animateToPage(index,
+              duration: const Duration(
+                milliseconds: 300,
+              ),
+              curve: Curves.ease);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            icon: const Icon(
+              Icons.apps,
+            ),
+            title: const Text(
+              'Jobs',
+            ),
+            activeColor: Colors.blue,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(
+              Icons.people,
+            ),
+            title: const Text(
+              'Profile',
+            ),
+            activeColor: Colors.purpleAccent,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(
+              Icons.logout,
+            ),
+            title: const Text(
+              'Logout',
+            ),
+            activeColor: Colors.red,
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
