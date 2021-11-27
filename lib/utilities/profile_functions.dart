59a1de37ac27bda6b5497096a5922f00b270d678
Kamran <56407135+k5924@users.noreply.github.com>
2021-11-27 22:39:17 +0000

    Created file to store functions which would create widgets dynamically on the profile page

---
+++ lib/utilities/profile_functions.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+getFirmDetails(bool? freelance, String? firm) {
+  if (freelance != true) {
+    return Text(
+      "$firm",
+      style: const TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+        fontSize: 32,
+      ),
+    );
+  }
+}
+
+getNumber(String? number) {
+  if (number != null) {
+    return Text(
+      number,
+      style: const TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+        fontSize: 32,
+      ),
+    );
+  }
+}
