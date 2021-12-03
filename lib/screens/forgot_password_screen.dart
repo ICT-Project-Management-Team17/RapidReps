675728c04577fd1f6665f992216dfb76f100f8ba
Kamran <56407135+k5924@users.noreply.github.com>
2021-12-03 10:49:29 +0000

    Fixed background and buttons on forgot password page

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:rapid_reps/screens/export.dart';
-import 'package:rapid_reps/services/custom_toast.dart';
-import 'package:rapid_reps/widgets/custom_button.dart';
+import 'package:rapid_reps/services/export.dart';
+import 'package:rapid_reps/widgets/export.dart';
 
 class ForgotYourPassword extends StatefulWidget {
   const ForgotYourPassword({Key? key}) : super(key: key);
@@ -26,83 +26,88 @@ class _ForgotYourPasswordState extends State<ForgotYourPassword> {
         height: double.infinity,
         decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("assets/images/London.png"), fit: BoxFit.cover),
+            image: AssetImage("assets/images/london.png"),
+            fit: BoxFit.cover,
+          ),
         ),
-        alignment: Alignment.center,
-        child: SingleChildScrollView(
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 500,
-                width: 325,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25.0),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        const Text("Reset Password",
-                            style: TextStyle(fontSize: 36.0)),
-                        const SizedBox(
-                          height: 30,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 500,
+                    width: 325,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            const Text("Reset Password",
+                                style: TextStyle(fontSize: 36.0)),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            TextFormField(
+                              controller: currentEmailController,
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (email) {
+                                if (email!.isEmpty) {
+                                  return "Please enter your email";
+                                }
+                                // Email validation
+                                if (!RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(email)) {
+                                  return "Please enter a valid email";
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                currentEmailController.text = value!;
+                              },
+                              decoration: const InputDecoration(
+                                  hintText: 'Email',
+                                  hintStyle: TextStyle(color: Colors.grey)),
+                            ),
+                            const SizedBox(
+                              height: 25,
+                            ),
+                            CustomButton(
+                              buttonColour: const Color(0xFF951B81),
+                              horizontalPadding: 70,
+                              buttonText: 'Reset',
+                              onPressed: () async {
+                                sendPasswordResetEmail();
+                              },
+                            ),
+                            const SizedBox(
+                              height: 25,
+                            ),
+                            CustomButton(
+                              buttonColour: const Color(0xFFF49413),
+                              horizontalPadding: 70,
+                              buttonText: 'Back',
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
                         ),
-                        TextFormField(
-                          controller: currentEmailController,
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (email) {
-                            if (email!.isEmpty) {
-                              return "Please enter your email";
-                            }
-                            // Email validation
-                            if (!RegExp(
-                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                .hasMatch(email)) {
-                              return "Please enter a valid email";
-                            }
-                            return null;
-                          },
-                          onSaved: (value) {
-                            currentEmailController.text = value!;
-                          },
-                          decoration: const InputDecoration(
-                              hintText: 'Email',
-                              hintStyle: TextStyle(color: Colors.grey)),
-                        ),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        CustomButton(
-                          buttonColour: const Color(0xFF951B81),
-                          horizontalPadding: 70,
-                          buttonText: 'Reset',
-                          onPressed: () async {
-                            sendPasswordResetEmail();
-                          },
-                        ),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        CustomButton(
-                          buttonColour: const Color(0xFFF49413),
-                          horizontalPadding: 70,
-                          buttonText: 'Back',
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
@@ -122,9 +127,14 @@ class _ForgotYourPasswordState extends State<ForgotYourPassword> {
         );
       } on FirebaseAuthException catch (error) {
         errorText = error.code;
-        // if (errorText == 'Sorry we could not find your accout') {}
-        // customToast(msg: errorText!, backgroundColor: Colors.red);
-        print(errorText);
+        if (errorText == 'user-not-found') {
+          errorText = 'Incorrect Email';
+        }
+        // Flutter toast used to show the 'incorrect password' error
+        customToast(
+          msg: errorText!,
+          backgroundColor: Colors.red,
+        );
       }
     }
   }
