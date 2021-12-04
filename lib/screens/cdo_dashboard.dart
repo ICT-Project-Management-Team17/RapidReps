fce9901311c580a664fb06171d37e70597f3853c
Kamran <56407135+k5924@users.noreply.github.com>
2021-12-04 17:43:36 +0000

    Implemented NavBar Refactor for CDO

--- lib/screens/cdo_dashboard.dart
+++ lib/screens/cdo_dashboard.dart
@@ -275,20 +275,10 @@ class _CDODashboardState extends State<CDODashboard> {
             ],
           ),
         ),
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: _currentIndex,
-          showElevation: true,
-          itemCornerRadius: 24,
-          curve: Curves.easeIn,
-          onItemSelected: (index) {
-            setState(() => _currentIndex = index);
-            _pageController.animateToPage(index,
-                duration: const Duration(
-                  milliseconds: 300,
-                ),
-                curve: Curves.ease);
-          },
-          items: <BottomNavyBarItem>[
+        bottomNavigationBar: CustomNavBar(
+          controller: _pageController,
+          currentIndex: _currentIndex,
+          navBarItems: <BottomNavyBarItem>[
             BottomNavyBarItem(
               icon: const Icon(
                 Icons.apps,
@@ -325,5 +315,3 @@ class _CDODashboardState extends State<CDODashboard> {
     );
   }
 }
-
-
